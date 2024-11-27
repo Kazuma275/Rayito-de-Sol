7d170dfbd289f3f345df104dd8274d019b52458d
Kazuma275 <sergioramosalarcon@gmail.com>
2024-11-27 17:44:47 +0100

    conexion a bbdd

---
+++ conection.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost"; // O la dirección de tu servidor de base de datos
+$username = "sergio"; // Usuario de la base de datos
+$password = "almaysergio"; // Contraseña del usuario
+$dbname = "rayito_db"; // Nombre de la base de datos
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar conexión
+if ($conn->connect_error) {
+    die("La conexión ha fallado: " . $conn->connect_error);
+}
+echo "Conexión exitosa";
+?>
