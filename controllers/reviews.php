a8f7c82a6c17e0b71da659f84b7b0d37ea9522bd
Kazuma275 <sergioramosalarcon@gmail.com>
2024-11-28 12:43:09 +0100

    Depuracion BBDD

--- controllers/reviews.php
+++ controllers/reviews.php
@@ -1,7 +1,7 @@
 <?php
 require_once "./controllers/conection.php"; // Asegúrate de que la conexión esté configurada correctamente
 
-$sql = "SELECT author, review_text FROM reviews"; // Cambié "reviews_table" por "reviews"
+$sql = "SELECT author, review_text, author_image, review_date FROM reviews";
 $result = $conn->query($sql);
 
 if ($result === false) {
@@ -11,8 +11,10 @@ if ($result === false) {
 if ($result->num_rows > 0) {
     echo "Consulta exitosa. Hay resultados:<br>";
     while ($row = $result->fetch_assoc()) {
-        echo "Autor: " . htmlspecialchars($row['author']) . "<br>";
+        echo "Autor: " . htmlspecialchars($row['author_name']) . "<br>";
         echo "Reseña: " . htmlspecialchars($row['review_text']) . "<br>";
+        echo "" . htmlspecialchars($row['author_image']) . "<br>";
+        echo "Reseña: " . htmlspecialchars($row['review_date']) . "<br>";
     }
 } else {
     echo "No se encontraron resultados.";
