346b2eba111b578d326a272299bc8b27579667e7
Kazuma275 <sergioramosalarcon@gmail.com>
2024-11-28 08:41:11 +0100

    ARRAYS languague

---
+++ lang/en.php
@@ -0,0 +1,19 @@
+<?php
+$lang = [
+    'title' => 'Rayito de Sol',
+    'home' => 'Home',
+    'amenities' => 'Amenities',
+    'gallery' => 'Gallery',
+    'reviews' => 'Reviews',
+    'contact' => 'Contact',
+    'booking' => 'Booking',
+    'account' => 'Account',
+    'signup' => 'Sign Up',
+    'signup_message' => 'For any questions or more information, please reach out to us at',
+    'username' => 'Username',
+    'password' => 'Password',
+    'register' => 'Register',
+    'current_lang' => 'English',
+    'rights' => 'All rights reserved.',
+];
+?>
