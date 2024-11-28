a496b4955cfc4888461cfd57b67d9811bea8fcb4
Kazuma275 <sergioramosalarcon@gmail.com>
2024-11-28 19:46:35 +0100

    Update register.php

--- build/register.php
+++ build/register.php
@@ -2,8 +2,6 @@
 require_once "../assets/classes/User.php";
 require_once "../controllers/conection.php";
 
-session_start();
-
 // Check if the form has been submitted
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = trim($_POST['username']); // Recoge el valor del campo 'username'
@@ -26,11 +24,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     // Ejecutar la consulta y verificar si se ejecutó correctamente
     if ($conn->query($sql) === TRUE) {
-        // Guarda los datos en la sesión para mostrarlos en la página
-        $_SESSION['usernameValue'] = $usernameValue;
-        $_SESSION['passwordValue'] = $passwordValue;
-        header("Location: ./tests.php");
-        exit();
+        echo "User registered successfully!";
     } else {
         echo "Error registering user: " . $conn->error;
     }
