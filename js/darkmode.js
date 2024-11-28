841d3b3cc438d4053871201cf447848be5d1709a
Kazuma275 <sergioramosalarcon@gmail.com>
2024-11-28 12:16:22 +0100

    Dark Mode

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,4 +1,4 @@
-// Modo Oscuro/Claro
+// Selecciona el botón de cambio de modo oscuro
 const toggleDarkMode = document.getElementById("toggle-dark-mode");
 
 // Verifica si el modo oscuro está activo a través de la clase en el body
@@ -9,21 +9,18 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 });
 
+// Evento para alternar el modo oscuro
 toggleDarkMode.addEventListener("click", () => {
     // Alterna la clase dark-mode en el body
     document.body.classList.toggle("dark-mode");
     
-    // Cambia el icono de la luna a sol
+    // Cambia el icono de la luna a sol y viceversa
     const icon = toggleDarkMode.querySelector("i");
     if (document.body.classList.contains("dark-mode")) {
         icon.classList.remove("fa-moon");
         icon.classList.add("fa-sun");
-        // Redirige para guardar el estado en la sesión
-        window.location.href = "?toggle_dark_mode=true";
     } else {
         icon.classList.remove("fa-sun");
         icon.classList.add("fa-moon");
-        // Redirige para guardar el estado en la sesión
-        window.location.href = "?toggle_dark_mode=true";
     }
 });
