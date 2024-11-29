8436095f79da79dd532887bb5536f417a7b78bad
Kazuma275 <sergioramosalarcon@gmail.com>
2024-11-29 13:53:26 +0100

    Update ubication.php

--- controllers/ubication.php
+++ controllers/ubication.php
@@ -1,5 +1,8 @@
 <?php include './language.php'; // Incluye el archivo para definir $lang ?>
-<?php echo 'Idioma detectado: ' . $lang; // Línea de depuración ?>
+<?php
+// Prueba con un valor de idioma estático
+$lang = 'es';
+?>
 <div id="iframe">
     <iframe 
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d801.9384161512957!2d-4.718110630417203!3d36.48766537649933!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd731f8e245ede8f%3A0xa37947aa87978cdd!2sCl.%20Alhamar%2C%2010%2C%2029649%20Mijas%2C%20M%C3%A1laga!5e0!3m2!1es!2ses!4v1732882714315!5m2!1es!2ses&hl=<?php echo $lang; ?>"
@@ -11,6 +14,7 @@
         referrerpolicy="no-referrer-when-downgrade">
     </iframe>
 </div>
+
 
 
 <!-- Cargar la API de Google Maps con la clave de API -->
