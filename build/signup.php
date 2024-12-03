718406e1de6585b32d8b3b9c6fde1ea1ee1641f9
Kazuma275 <sergioramosalarcon@gmail.com>
2024-12-03 21:03:13 +0100

    Update signup.php

--- build/signup.php
+++ build/signup.php
@@ -52,43 +52,44 @@
         <div class="container">
             <!-- Navbar -->
             <nav>
-                <div class="topnav" id="myTopnav">
-                    <a href="../index.php#parallax-section" class="active"><?php echo $lang['home']; ?></a>
-                    <a href="../index.php#amenities"><?php echo $lang['amenities']; ?></a>
-                    <a href="../index.php#gallery"><?php echo $lang['gallery']; ?></a>
-                    <a href="../index.php#reviews"><?php echo $lang['reviews']; ?></a>
-                    <a href="#"><?php echo $lang['account'];?></a>
-                    <?php if (isset($_SESSION['username']) && $_SESSION['logged_in'] === true): ?>
-                    <!-- Mostrar el enlace de reservas solo si la sesión está activa -->
-                        <a href="/build/create.php"><?php echo $lang['make_reservation']?></a>
-                    <?php endif; ?>
-                    <?php if (isset($_SESSION['username']) && $_SESSION['logged_in'] === true): ?>
-                        <a class="login-message"><?php echo "Hey," . $_SESSION['username']?></a>
-                    <?php endif; ?>
+            <div class="topnav" id="myTopnav">
+                <a href="#parallax-section" class="active">Home</a>
+                <a href="/index.php#amenities"><?php echo $lang['amenities']?></a>
+                <a href="/index.php#gallery"><?php echo $lang['gallery']?></a>
+                <a href="/index.php#reviews"><?php echo $lang['reviews']?></a>
+                <a href="/index.php#ubication"><?php echo $lang['ubication']; ?></a>
+                <a href="./build/signup.php"><?php echo $lang['account']?></a>
+                <?php if (isset($_SESSION['username']) && $_SESSION['logged_in'] === true): ?>
+                <!-- Mostrar el enlace de reservas solo si la sesión está activa -->
+                    <a href="/create.php"><?php echo $lang['make_reservation']?></a>
+                <?php endif; ?>
+                <?php if (isset($_SESSION['username']) && $_SESSION['logged_in'] === true): ?>
+                    <a href="/information.php" class="login-message"><?php echo "Hey," . $_SESSION['username']?></a>
+                <?php endif; ?>
 
-                    <!-- Contenedor para la bandera y el modo oscuro -->
-                    <div class="settings-container" style="position: relative;">
-                        <!-- Selector de idioma -->
-                        <div class="language-selector">
-                            <img id="current-flag" src="../img/idiomas/<?php echo isset($_SESSION['lang']) ? $_SESSION['lang'] : 'es'; ?>.png" alt="<?php echo $lang['current_lang'] ?? 'Español'; ?>" class="flag">
-                            <ul class="language-menu">
-                                <li><a href="?lang=en" data-lang="en"><img src="../img/idiomas/en.png" alt="English" class="flag-preview"></a></li>
-                                <li><a href="?lang=fr" data-lang="fr"><img src="../img/idiomas/fr.png" alt="Français" class="flag-preview"></a></li>
-                                <li><a href="?lang=es" data-lang="es"><img src="../img/idiomas/es.png" alt="Español" class="flag-preview"></a></li>
-                                <li><a href="?lang=cn" data-lang="cn"><img src="../img/idiomas/cn.png" alt="中国人" class="flag-preview"></a></li>
-                                <li><a href="?lang=it" data-lang="it"><img src="../img/idiomas/it.png" alt="Italiano" class="flag-preview"></a></li>
-                                <li><a href="?lang=br" data-lang="br"><img src="../img/idiomas/br.png" alt="Brasileiro" class="flag-preview"></a></li>
-                                <li><a href="?lang=ua" data-lang="ua"><img src="../img/idiomas/ua.png" alt="українська" class="flag-preview"></a></li>
-                                <li><a href="?lang=ru" data-lang="ru"><img src="../img/idiomas/ru.png" alt="Русский" class="flag-preview"></a></li>
-                            </ul>
-                        </div>
-
-                        <!-- Botón de Modo Oscuro/Claro -->
-                        <button id="toggle-dark-mode" class="dark-mode-toggle">
-                            <i class="fa fa-moon"></i>
-                        </button>
+                <!-- Contenedor para la bandera y el modo oscuro -->
+                <div class="settings-container" style="position: relative;">
+                    <!-- Selector de idioma -->
+                    <div class="language-selector">
+                        <img id="current-flag" src="../img/idiomas/<?php echo isset($_SESSION['lang']) ? $_SESSION['lang'] : 'es'; ?>.png" alt="<?php echo $lang['current_lang'] ?? 'Español'; ?>" class="flag">
+                        <ul class="language-menu">
+                            <li><a href="?lang=en" data-lang="en"><img src="../img/idiomas/en.png" alt="English" class="flag-preview"></a></li>
+                            <li><a href="?lang=fr" data-lang="fr"><img src="../img/idiomas/fr.png" alt="Français" class="flag-preview"></a></li>
+                            <li><a href="?lang=es" data-lang="es"><img src="../img/idiomas/es.png" alt="Español" class="flag-preview"></a></li>
+                            <li><a href="?lang=cn" data-lang="cn"><img src="../img/idiomas/cn.png" alt="中国人" class="flag-preview"></a></li>
+                            <li><a href="?lang=it" data-lang="it"><img src="../img/idiomas/it.png" alt="Italiano" class="flag-preview"></a></li>
+                            <li><a href="?lang=br" data-lang="br"><img src="../img/idiomas/br.png" alt="Brasileiro" class="flag-preview"></a></li>
+                            <li><a href="?lang=ua" data-lang="ua"><img src="../img/idiomas/ua.png" alt="українська" class="flag-preview"></a></li>
+                            <li><a href="?lang=ru" data-lang="ru"><img src="../img/idiomas/ru.png" alt="Русский" class="flag-preview"></a></li>
+                        </ul>
                     </div>
+
+                    <!-- Botón de Modo Oscuro/Claro -->
+                    <button id="toggle-dark-mode" class="dark-mode-toggle">
+                        <i class="fa fa-moon"></i>
+                    </button>
                 </div>
+			</div>
             </nav>
             
             <!-- Sección "Sign Up" -->
