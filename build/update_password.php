53992cfb725b57aa2ecdec7bfc5fda242035d708
Kazuma275 <sergioramosalarcon@gmail.com>
2024-12-03 20:50:06 +0100

    Depuración

--- build/update_password.php
+++ build/update_password.php
@@ -1,7 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 // Asegúrate de iniciar la sesión para obtener los datos del usuario
 session_start();
 
