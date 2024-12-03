7c032a46a21c16752315d4a4b1701f53cf24aae3
Kazuma275 <sergioramosalarcon@gmail.com>
2024-12-03 19:12:48 +0100

    Update create.php

--- build/crud/create.php
+++ build/crud/create.php
@@ -38,41 +38,47 @@ $password = $_SESSION['passwordValue'];
     <title><?php echo $lang['title']; ?></title>
 
     <!-- CSS -->
-    <link rel="stylesheet" href="../../css/style.css">
+    <link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.css">
 
     <!-- Favicon -->
-    <link rel="icon" href="../../img/favicon.png" type="image/x-icon">
+    <link rel="icon" href="./img/favicon.png" type="image/x-icon">
 
     <!-- JS -->
-    <script defer src="../../js/javascript.js"></script>
-    <script defer src="../../js/darkmode.js"></script>
-    <script defer src="../../js/languague.js"></script>
-    <script defer src="../../js/eye.js"></script>
+    <script defer src="/js/javascript.js"></script>
+    <script defer src="/js/darkmode.js"></script>
+    <script defer src="/js/languague.js"></script>
+    <script defer src="/js/eye.js"></script>
 </head>
 <body>
     <div class="container">
         <!-- Navbar -->
         <nav>
             <div class="topnav" id="myTopnav">
-                <a href="../../index.php#parallax-section" class="active"><?php echo $lang['home']; ?></a>
-                <a href="../../index.php#amenities"><?php echo $lang['amenities']; ?></a>
-                <a href="../../index.php#gallery"><?php echo $lang['gallery']; ?></a>
-                <a href="../../index.php#reviews"><?php echo $lang['reviews']; ?></a>
-                <a href="../../index.php#contact"><?php echo $lang['contact']; ?></a>
-                <a href="../../index.php#reservation"><?php echo $lang['booking']; ?></a>
+                <a href="/index.php#parallax-section" class="active"><?php echo $lang['home']; ?></a>
+                <a href="/index.php#amenities"><?php echo $lang['amenities']; ?></a>
+                <a href="/index.php#gallery"><?php echo $lang['gallery']; ?></a>
+                <a href="/index.php#reviews"><?php echo $lang['reviews']; ?></a>
+                <a href="/index.php#contact"><?php echo $lang['contact']; ?></a>
+                <a href="/index.php#reservation"><?php echo $lang['booking']; ?></a>
                 <a href="#"><?php echo $lang['account']; ?></a>
-                <a href="./../crud/create.php">Haz una reserva</a>
+                <a href="/crud/create.php">Haz una reserva</a>
 
                 <!-- Contenedor para la bandera y el modo oscuro -->
-                <div class="settings-container">
+                <!-- Contenedor para la bandera y el modo oscuro -->
+                <div class="settings-container" style="position: relative;">
                     <!-- Selector de idioma -->
                     <div class="language-selector">
-                        <img id="current-flag" src="../../img/idiomas/<?php echo $_SESSION['lang']; ?>.png" alt="<?php echo $lang['current_lang']; ?>" class="flag">
+                        <img id="current-flag" src="/img/idiomas/<?php echo isset($_SESSION['lang']) ? $_SESSION['lang'] : 'es'; ?>.png" alt="<?php echo $lang['current_lang'] ?? 'Español'; ?>" class="flag">
                         <ul class="language-menu">
-                            <li><a href="?lang=en" data-lang="en"><img src="../img/idiomas/en.png" alt="English" class="flag-preview"></a></li>
-                            <li><a href="?lang=fr" data-lang="fr"><img src="../img/idiomas/fr.png" alt="Français" class="flag-preview"></a></li>
-                            <li><a href="?lang=es" data-lang="es"><img src="../img/idiomas/es.png" alt="Español" class="flag-preview"></a></li>
+                            <li><a href="?lang=en" data-lang="en"><img src="/img/idiomas/en.png" alt="English" class="flag-preview"></a></li>
+                            <li><a href="?lang=fr" data-lang="fr"><img src="/img/idiomas/fr.png" alt="Français" class="flag-preview"></a></li>
+                            <li><a href="?lang=es" data-lang="es"><img src="/img/idiomas/es.png" alt="Español" class="flag-preview"></a></li>
+                            <li><a href="?lang=cn" data-lang="cn"><img src="/img/idiomas/cn.png" alt="中国人" class="flag-preview"></a></li>
+                            <li><a href="?lang=it" data-lang="it"><img src="/img/idiomas/it.png" alt="Italiano" class="flag-preview"></a></li>
+                            <li><a href="?lang=br" data-lang="br"><img src="/img/idiomas/br.png" alt="Brasileiro" class="flag-preview"></a></li>
+                            <li><a href="?lang=ua" data-lang="ua"><img src="/img/idiomas/ua.png" alt="українська" class="flag-preview"></a></li>
+                            <li><a href="?lang=ru" data-lang="ru"><img src="/img/idiomas/ru.png" alt="Русский" class="flag-preview"></a></li>
                         </ul>
                     </div>
 
