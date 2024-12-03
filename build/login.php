0e2955c3f61d8e71f24c9fd033387966c204d4b6
Kazuma275 <sergioramosalarcon@gmail.com>
2024-12-03 08:56:05 +0100

    Update login.php

--- build/login.php
+++ build/login.php
@@ -48,7 +48,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     $_SESSION['logged_in'] = true;
 
                     // Redirige al index
-                    header("Location: ./index.php");
+                    header("Location: ../index.php");
 
                     exit();
                 } else {
