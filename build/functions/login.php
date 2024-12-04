102ebf6abf987cfb26a41dd68f0ba3dd49d4e863
Kazuma275 <sergioramosalarcon@gmail.com>
2024-12-04 19:53:10 +0100

    Update login.php

--- build/functions/login.php
+++ build/functions/login.php
@@ -1,8 +1,9 @@
 <?php
 
-/* DEPURACIÓN */
-/* ini_set('display_errors', 1);
-error_reporting(E_ALL); */
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 
 session_start();
 
