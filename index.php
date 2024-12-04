1a88898d020b1530f13d8b6dc4e5d87c4670ef03
Kazuma275 <sergioramosalarcon@gmail.com>
2024-12-04 21:23:39 +0100

    Update index.php

--- index.php
+++ index.php
@@ -189,8 +189,8 @@ if (file_exists($lang_file)) {
         <div class="footer-links">
             <a href="#" class="active"><?php echo $lang['home']; ?></a>
             <a href="#amenities"><?php echo $lang['amenities']; ?></a>
-            <a href="#reviews"><?php echo $lang['reviews']; ?></a>
             <a href="#gallery"><?php echo $lang['gallery']; ?></a>
+            <a href="#reviews"><?php echo $lang['reviews']; ?></a>
             <a href="/build/functions/signup.php"><?php echo $lang['account']; ?></a>
         </div>
 
