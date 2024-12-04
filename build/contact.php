4106f42c7c669c420ebd9cb51ce79211f6f482eb
Kazuma275 <sergioramosalarcon@gmail.com>
2024-12-04 17:39:07 +0100

    Update contact.php

--- build/contact.php
+++ build/contact.php
@@ -97,8 +97,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 <?php if (isset($_SESSION['username']) && $_SESSION['logged_in'] === true): ?>
                 <!-- Mostrar el enlace de reservas solo si la sesión está activa -->
                     <a href="/build/create.php"><?php echo $lang['make_reservation']?></a>
-                    <a href="/build/information.php" class="login-message"><?php echo "Hey," . $_SESSION['username']?></a>
+                <?php endif; ?>
+                <?php if (isset($_SESSION['username']) && $_SESSION['logged_in'] === true): ?>
+                <!-- Mostrar el enlace de reservas solo si la sesión está activa -->
                     <a href="/build/contact.php">Contacta</a>
+                <?php endif; ?>
+                <?php if (isset($_SESSION['username']) && $_SESSION['logged_in'] === true): ?>
+                    <a href="/build/information.php" class="login-message"><?php echo "Hey," . $_SESSION['username']?></a>
                 <?php endif; ?>
 
                 <!-- Contenedor para la bandera y el modo oscuro -->
