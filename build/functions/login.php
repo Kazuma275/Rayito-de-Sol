c1578b5a96fd41552833362ba53e0f818b10c600
Kazuma275 <sergioramosalarcon@gmail.com>
2024-12-04 19:59:31 +0100

    Update login.php

--- build/functions/login.php
+++ build/functions/login.php
@@ -9,15 +9,6 @@ session_start();
 
 $session_lifetime = 180; // Tiempo de vida de la sesión en segundos
 
-// Verifica si la sesión tiene un tiempo de expiración configurado
-if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > $session_lifetime)) {
-    // La sesión ha expirado
-    session_unset();     // Borra todas las variables de sesión
-    session_destroy();   // Destruye la sesión
-    header("Location: __DIR__/index.php?session_expired=true"); // Redirige a una página o muestra un mensaje
-    exit();
-}
-$_SESSION['LAST_ACTIVITY'] = time(); // Actualiza el tiempo de la última actividad
 
 require_once __DIR__ . "/../../controllers/conection.php";  
 
