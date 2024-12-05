81a8244bcdc96fb6ade5b8df7e261eb37fe448e1
Kazuma275 <sergioramosalarcon@gmail.com>
2024-12-05 09:35:52 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,16 @@ session_start();
 // Define un idioma predeterminado
 $default_lang = 'es';
 
-require_once __DIR__. "/../../controllers/conection.php";
+$session_lifetime = 360;
+// Verifica si la sesión tiene un tiempo de expiración configurado
+if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > $session_lifetime)) {
+    // La sesión ha expirado
+    session_unset();     // Borra todas las variables de sesión
+    session_destroy();   // Destruye la sesión
+    header("Location: /index.php?session_expired=true"); // Redirige a la página index.php en la raíz del proyecto
+    exit();
+}
+$_SESSION['LAST_ACTIVITY'] = time(); // Actualiza el tiempo de la última actividad
 
 // Obtén el idioma de la URL o de la sesión
 if (isset($_GET['lang'])) {
