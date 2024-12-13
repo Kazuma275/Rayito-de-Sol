96e05f2ad8f2d5da50fa7d2db0631acdccd332b3
Kazuma275 <sergioramosalarcon@gmail.com>
2024-12-13 14:13:21 +0100

    Back again

--- js/navbar.js
+++ js/navbar.js
@@ -1,9 +1,9 @@
 function toggleNavbar() {
     var nav = document.getElementById("myTopnav");
     // Alternamos la clase 'responsive' para mostrar/ocultar los enlaces
-    if (nav.className.indexOf("responsive") === -1) {
-        nav.className += " responsive";  // Si 'responsive' no está presente, la agregamos
+    if (nav.className === "topnav") {
+        nav.className += " responsive";  // Agregamos la clase 'responsive' que activa la visualización de los enlaces
     } else {
-        nav.className = "topnav";  // Si 'responsive' está presente, la eliminamos
+        nav.className = "topnav";  // Removemos la clase 'responsive', ocultando los enlaces
     }
 }
