ac6a21ad03a45cc3c6931f91e24855e987d4c41e
Kazuma275 <sergioramosalarcon@gmail.com>
2024-12-13 10:52:08 +0100

    Update darkmode.js

--- js/darkmode.js
+++ js/darkmode.js
@@ -17,7 +17,3 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
-
-$('.dayNight input').change(function () {
-    $('body').toggleClass('day', $(this).is(':checked'));
-});
