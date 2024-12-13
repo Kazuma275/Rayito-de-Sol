f24d802a859521e1bc9b339c52d23994c7c67ac6
Kazuma275 <sergioramosalarcon@gmail.com>
2024-12-13 12:16:49 +0100

    Update darkmode.js

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,10 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     const toggleCheckbox = document.querySelector('.dayNight input');
     const topNav = document.querySelector('.topnav'); // Selecciona la navbar
-    const reviewsContainer = document.querySelector('.reviews-section'); // Contenedor de reviews
+    const amenities = document.querySelector('#amenities'); // Amenidades
     const gallery = document.querySelector('#gallery'); // Contenedor de galleria
+    const reviewsContainer = document.querySelector('.reviews-section'); // Contenedor de reviews
+    const ubication = document.querySelector('#ubication'); // Ubicación
     const footer = document.querySelector('.footer'); // Footer
-    const amenities = document.querySelector('#amenities'); // Footer
 
 
     toggleCheckbox.addEventListener('change', () => {
@@ -14,18 +15,22 @@ document.addEventListener('DOMContentLoaded', () => {
             // Aplica estilos para modo oscuro
             topNav.style.backgroundColor = '#121212'; // Fondo negro
             topNav.style.color = '#ffffff'; // Texto blanco
+
+            amenities.style.backgroundColor = '#000000'; // Fondo negro
+            amenities.style.color = '#ffffff'; // Texto blanco
+
+            gallery.style.backgroundColor = '#2F3843'; // Fondo oscuro
+            gallery.style.style.color = '#dddddd'; // Texto
             
             reviewsContainer.style.backgroundColor = '#20252D'; // Fondo oscuro
             reviewsContainer.style.color = '#dddddd'; // Texto claro
-            
-            gallery.style.backgroundColor = '#2F3843'; // Fondo oscuro
-            gallery.style.style.color = '#dddddd'; // Texto
+
+            ubication.style.backgroundColor = '#000000'; // Fondo
+            ubication.style.color = '#ffffff'; // Texto blanco
 
             footer.style.backgroundColor = '#000000'; // Fondo negro
             footer.style.color = '#ffffff'; // Texto blanco
 
-            amenities.style.backgroundColor = '#000000'; // Fondo negro
-            amenities.style.color = '#ffffff'; // Texto blanco
 
         } else {
             document.body.classList.remove('dark-mode');
@@ -36,13 +41,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
             amenities.style.backgroundColor = '#D8E4F3'; // Fondo claro
             amenities.style.color = '#000000'; // Texto oscuro
+            
+            gallery.style.backgroundColor = '#E2EBF6'; // Fondo claro
+            gallery.style.color = '#000000'; // Texto oscuro
 
             // Estilos para reviews-container en modo claro
             reviewsContainer.style.backgroundColor = '#ECF1F9'; // Fondo claro
             reviewsContainer.style.color = '#000000'; // Texto oscuro
-        
-            gallery.style.backgroundColor = '#E2EBF6'; // Fondo claro
-            gallery.style.color = '#000000'; // Texto oscuro
+
+            ubication.style.backgroundColor = '#F5F8FC'; // Fondo claro
+            ubication.style.color = '#000000'; // Texto oscuro
 
             footer.style.backgroundColor = '#FCFCFC'; // Fondo claro
             footer.style.color = '#000000'; // Texto oscuro
