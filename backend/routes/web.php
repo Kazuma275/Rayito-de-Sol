e69e54d0698eeaca3480759e0ef4eb51bd58313a
Kazuma275 <sergioramosalarcon@gmail.com>
2025-06-08 11:43:16 +0200

    Update web.php

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Events\MensajeEnviado;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/probar-broadcast', function () {
+    broadcast(new MensajeEnviado('¡Hola desde Laravel y Pusher!'));
+    return 'Evento enviado';
 });
