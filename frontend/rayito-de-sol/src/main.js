4708a6284dfa42f8570e9632afd95f29c498dbb9
Kazuma275 <sergioramosalarcon@gmail.com>
2025-06-08 11:48:47 +0200

    Update main.js
    import echo

--- frontend/rayito-de-sol/src/main.js
+++ frontend/rayito-de-sol/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/routes'
 import { createPinia } from 'pinia'
+import './echo.js';
 
 const app = createApp(App)
 app.use(router) 
